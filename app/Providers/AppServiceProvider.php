391bfefd8f62fecbccbfe2cc0be289a31688d3c3
hemaabdo <hemaabdoo390@gmail.com>
2024-03-24 10:11:18 +0200

    intial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Filament\Notifications\Livewire\DatabaseNotifications;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,9 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        DatabaseNotifications::trigger('filament.notifications.database-notifications-trigger');
+
+
     }
 
     /**
